6ade15902cecfee199c71c763a5f9dfc96d74ae6
Arnab Das <das92.arnab@gmail.com>
2019-07-28 22:08:57 +0530

    base.OnConfiguring() and base.OnModelCreating() are empty methods. Not necessary to call them.

--- In.GovernanceRating.Repository/ApplicationContext.cs
+++ In.GovernanceRating.Repository/ApplicationContext.cs
@@ -19,12 +19,12 @@ namespace In.GovernanceRating.Repository
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            
         }
     }
 }
